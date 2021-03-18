470225a2509915e7b9c6ae13c7b9d869a2c5298d
Jie Yu You <jy_you@hotmail.com>
2021-03-18 22:27:20 +0100

    [This comment] Adds json dependant greeting.

--- Butler.cs
+++ Butler.cs
@@ -1,18 +1,40 @@
 ﻿using System.IO;
+using System.Text.Json;
 
 namespace ChatButler
 {
     public class Butler
     {
+        private Greetings _greetings;
+
         public Butler()
         {
             // json file inlezen
             string json = File.ReadAllText("greetings.json");
+            _greetings = JsonSerializer.Deserialize<Greetings>(json);
         }
 
         public string Greet()
         {
-            return "Hi";
+            if(_greetings.language.Equals("nl"))
+            {
+                return _greetings.nl;
+            }
+
+            else if(_greetings.language.Equals("en"))
+            {
+                return _greetings.en;
+            }
+
+            return string.Empty; 
+        }
+
+        public class Greetings
+        {
+            public string language { get; set; }
+            public string nl { get; set; }
+            public string en { get; set; }
+
         }
     }
 }
